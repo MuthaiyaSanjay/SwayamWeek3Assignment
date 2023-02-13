387c585a77fa5bc4dd76cccc848c9cb61a1966fd
Muthaiya <108641837+MuthaiyaSanjay@users.noreply.github.com>
2023-02-13 22:41:21 +0530

    Add files via upload
    The Two programs to write,execute and passes the Test Cases in swayam course.

---
+++ parallelogram.py
@@ -0,0 +1,12 @@
+columns=int(input("Enter Rows Value"))
+rows=int(input("Enter the Column Values"))
+print('')
+for i in range(columns):
+	for j in range(rows):
+		print('*', end='')
+	print('')
+
+# The Code will Produce the Output
+#	****
+#	****
+#	****
